238c2e7350ae688beb0925cc9c2befe3d897c3d0
Thanxs <nikolskiy90@gmail.com>
2021-12-07 00:02:29 +0200

    Updates

--- static/js/3.de606bb9.chunk.js
+++ static/js/3.de606bb9.chunk.js
@@ -1,2 +1,2 @@
 (this["webpackJsonpposts-app"]=this["webpackJsonpposts-app"]||[]).push([[3],{77:function(t,c,s){},78:function(t,c,s){"use strict";s.r(c);s(77);var e=s(0);c.default=function(){var t=Array.from({length:11}).map((function(t,c){return c+1}));return Object(e.jsx)("div",{className:"image-collection",children:t.map((function(t,c){return Object(e.jsx)("div",{className:"image-collection__item",children:Object(e.jsx)("img",{width:"350",height:"220",src:"".concat("/posts-app","/assets/images/").concat(t,".jpeg"),alt:"pic"})},c)}))})}}}]);
-//# sourceMappingURL=3.1295d70b.chunk.js.map
+//# sourceMappingURL=3.de606bb9.chunk.js.map
